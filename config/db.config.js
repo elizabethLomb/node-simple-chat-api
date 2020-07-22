2438c2d8e57e462461575284255fae9d2720c543
elizabethLomb <elizabeth.lombardi7@gmail.com>
2020-07-22 21:48:30 +0200

    build: db mongo config

---
+++ config/db.config.js
@@ -0,0 +1,14 @@
+require('dotenv');
+
+const mongoose = require('mongoose');
+
+const MONGO_URI =
+  process.env.MONGO_URI || 'mongodb://localhost:27017/node-chat-api';
+
+mongoose.connect(MONGO_URI);
+mongoose.connection.on('connected', () => {
+  console.info(`Connected to database: ${MONGO_URI}`);
+});
+mongoose.connection.on('error', (error) => {
+  console.error('Database connection error:', error);
+});
